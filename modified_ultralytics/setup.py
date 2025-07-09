fd8c4ce54e6608af29972b56596d275e495100a1
Rabih Chamas <rabihchamas2@gmail.com>
2025-07-09 18:34:11 +0200

    Include default.yaml in package

--- modified_ultralytics/setup.py
+++ modified_ultralytics/setup.py
@@ -3,23 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="ultralytics_lesions",
     version="0.1.0",
-    # packages=find_packages(),
-    # install_requires=[
-    #     "numpy>=1.23.0",
-    #     "matplotlib>=3.3.0",
-    #     "opencv-python>=4.6.0",
-    #     "pillow>=7.1.2",
-    #     "pyyaml>=5.3.1",
-    #     "requests>=2.23.0",
-    #     "scipy>=1.4.1",
-    #     "torch>=1.8.0",
-    #     "torchvision>=0.9.0",
-    #     "tqdm>=4.64.0",
-    #     "psutil",
-    #     "py-cpuinfo",
-    #     "pandas>=1.1.4",
-    #     "ultralytics-thop>=2.0.0",
-    # ],
-    python_requires='>=3.8',
-    # add other metadata as needed
+    packages=find_packages(),
+    include_package_data=True,  # ← this enables MANIFEST.in usage
+    package_data={
+        "ultralytics_lesions": ["cfg/*.yaml"],  # adjust path as needed
+    },
 )
